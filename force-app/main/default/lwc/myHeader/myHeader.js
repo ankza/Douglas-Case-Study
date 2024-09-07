b2978f1385ae99cfe8dc27813ab5e793da806180
Ankit Gupta <guptaankit711@gmail.com>
2024-09-07 20:10:10 +0200

    LWC

---
+++ force-app/main/default/lwc/myHeader/myHeader.js
@@ -0,0 +1,17 @@
+import { LightningElement,api } from 'lwc';
+
+export default class MyHeader extends LightningElement {
+    @api socialDetails
+   @api userDetails
+
+   get phoneLink(){
+    return 'tel:${this.userDetails.PHONE}'
+   }
+   get EmailLink(){
+    return 'mailto:${this.userDetails.Email}'
+   }
+   connectedCallback() {
+    console.log('socialDetails:', JSON.stringify(this.socialDetails, null, 2))
+}
+
+}
---
+++ force-app/main/default/lwc/mysummary/mysummary.js
@@ -0,0 +1,6 @@
+import { LightningElement ,api} from 'lwc';
+
+
+export default class Mysummary extends LightningElement {
+    @api details
+}
