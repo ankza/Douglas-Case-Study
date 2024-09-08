cb458b9e76e111fad74462c2e843d99906b3d3d9
Ankit Gupta <guptaankit711@gmail.com>
2024-09-08 18:16:30 +0200

    Comment Gupta added

--- force-app/main/default/lwc/myHeader/myHeader.js
+++ force-app/main/default/lwc/myHeader/myHeader.js
@@ -1,7 +1,7 @@
 import { LightningElement,api } from 'lwc';
 
 export default class MyHeader extends LightningElement {
-    /*comment*/
+    /*comment Gupta*/
     @api socialDetails
    @api userDetails
 
